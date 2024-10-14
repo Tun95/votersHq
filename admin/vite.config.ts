ea907a546220e7624e6f7ce483a04495f8bb3e1e
Tunji Akande <akandetunji3@gmail.com>
2024-10-14 22:42:29 +0100

    FEAT: voters saas

--- admin/vite.config.ts
+++ admin/vite.config.ts
@@ -2,6 +2,13 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: 'modern-compiler' // or "modern"
+      }
+    }
+  },
   plugins: [react()],
   build: {
     outDir: "dist",
