c9b9a86e1ff84e3605612dedd3e67e4251223ff1
Tunji Akande <akandetunji3@gmail.com>
2024-12-10 22:18:56 +0100

    FEAT: bills card clickable

--- frontend/src/components/bill and issue/MoreRelatedCards.tsx
+++ frontend/src/components/bill and issue/MoreRelatedCards.tsx
@@ -27,139 +27,144 @@ const MoreRelatedCards: React.FC<BillsResponse> = ({ fetchBill, bill }) => {
       <div className="related_cards_content">
         {" "}
         <div className="post_cards">
-          <div className="featured_card">
-            <div className="slider_content">
-              <div className="slide_list">
-                <div className="list">
-                  <div className="img_pallet">
-                    <div className="img">
-                      <span
-                        className="cursor"
-                        onClick={() => bill?.slug && handleLinkClick(bill.slug)}
-                      >
-                        <img src={bill?.image} alt={bill?.title} />
-                      </span>
-                    </div>
-                  </div>
-                  <div className="details">
-                    <div className="past_card">
-                      <div className="tag_date c_flex">
-                        <div className="tags">
-                          <div className="list a_flex">
-                            {bill?.sortCategory && (
-                              <span className="tag c_flex">
-                                <small className="bills_f_tag">
-                                  {bill?.sortCategory[0]}
-                                </small>
-                              </span>
-                            )}
-                            {bill?.sortStatus && (
-                              <span className="tag c_flex">
-                                <small className="bills_f_tag">
-                                  {bill?.sortStatus[0]}
-                                </small>
-                              </span>
-                            )}
-                          </div>
-                        </div>
-                        <div className="date">
-                          <small>
-                            {formatDateSlash(bill?.createdAt ?? "")}
-                          </small>
-                        </div>
-                      </div>
-                      <div className="candidate a_flex">
-                        <div className="img">
-                          <Link
-                            to={`/politician-profile-view/${bill?.user?._id}`}
-                          >
-                            <img
-                              className="user_candidate_img"
-                              src={bill?.candidates[0]?.image}
-                              alt={bill?.candidates[0]?.firstName}
-                            />
-                          </Link>
-                        </div>
-                        <div className="name_location">
-                          <div className="name">
-                            <TruncateMarkup lines={1}>
-                              <h5>
-                                <Link
-                                  to={`/politician-profile-view/${bill?.user?._id}`}
-                                >
-                                  {bill?.user?.lastName} {bill?.user?.firstName}
-                                </Link>
-                              </h5>
-                            </TruncateMarkup>
-                          </div>
-                          <div className="location">
-                            <TruncateMarkup lines={1}>
-                              <p>
-                                {bill?.user?.region},{" "}
-                                {bill?.user?.stateOfOrigin}
-                              </p>
-                            </TruncateMarkup>
-                          </div>
-                        </div>
+          <Link to={`/bills/${bill?.slug}`}>
+            <div className="featured_card">
+              <div className="slider_content">
+                <div className="slide_list">
+                  <div className="list">
+                    <div className="img_pallet">
+                      <div className="img">
+                        <span
+                          className="cursor"
+                          onClick={() =>
+                            bill?.slug && handleLinkClick(bill.slug)
+                          }
+                        >
+                          <img src={bill?.image} alt={bill?.title} />
+                        </span>
                       </div>
                     </div>
-                    <div className="name">
-                      <span className="cursor">
-                        <TruncateMarkup lines={2}>
-                          <h4
-                            onClick={() =>
-                              bill?.slug && handleLinkClick(bill.slug)
-                            }
-                          >
-                            {bill?.title}
-                          </h4>
-                        </TruncateMarkup>
-                      </span>
-                    </div>
+                    <div className="details">
+                      <div className="past_card">
+                        <div className="tag_date c_flex">
+                          <div className="tags">
+                            <div className="list a_flex">
+                              {bill?.sortCategory && (
+                                <span className="tag c_flex">
+                                  <small className="bills_f_tag">
+                                    {bill?.sortCategory[0]}
+                                  </small>
+                                </span>
+                              )}
+                              {bill?.sortStatus && (
+                                <span className="tag c_flex">
+                                  <small className="bills_f_tag">
+                                    {bill?.sortStatus[0]}
+                                  </small>
+                                </span>
+                              )}
+                            </div>
+                          </div>
+                          <div className="date">
+                            <small>
+                              {formatDateSlash(bill?.createdAt ?? "")}
+                            </small>
+                          </div>
+                        </div>
+                        <div className="candidate a_flex">
+                          <div className="img">
+                            <Link
+                              to={`/politician-profile-view/${bill?.user?._id}`}
+                            >
+                              <img
+                                className="user_candidate_img"
+                                src={bill?.candidates[0]?.image}
+                                alt={bill?.candidates[0]?.firstName}
+                              />
+                            </Link>
+                          </div>
+                          <div className="name_location">
+                            <div className="name">
+                              <TruncateMarkup lines={1}>
+                                <h5>
+                                  <Link
+                                    to={`/politician-profile-view/${bill?.user?._id}`}
+                                  >
+                                    {bill?.user?.lastName}{" "}
+                                    {bill?.user?.firstName}
+                                  </Link>
+                                </h5>
+                              </TruncateMarkup>
+                            </div>
+                            <div className="location">
+                              <TruncateMarkup lines={1}>
+                                <p>
+                                  {bill?.user?.region},{" "}
+                                  {bill?.user?.stateOfOrigin}
+                                </p>
+                              </TruncateMarkup>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="name">
+                        <span className="cursor">
+                          <TruncateMarkup lines={2}>
+                            <h4
+                              onClick={() =>
+                                bill?.slug && handleLinkClick(bill.slug)
+                              }
+                            >
+                              {bill?.title}
+                            </h4>
+                          </TruncateMarkup>
+                        </span>
+                      </div>
 
-                    <div className="past_card">
-                      <div className="description">
-                        <TruncateMarkup lines={3}>
-                          <p>{parse(bill?.description ?? "")}</p>
-                        </TruncateMarkup>
-                      </div>
-                      <div className="yea_nay">
-                        <div className="yea c_flex">
-                          <div className="tomb a_flex">
-                            <i className="fa-regular fa-thumbs-up"></i>
-                            <small>Yea</small>
-                          </div>
-                          <div className="progress_bar">
-                            <Line
-                              percent={bill?.yeaPercentage ?? 0}
-                              strokeWidth={1}
-                              trailWidth={1}
-                              strokeColor="var(--color-bg-green)"
-                            />
-                          </div>
-                          <div className="count_vote">
-                            <small>
-                              {formatNumberShort(bill?.totalYeaVotes ?? 0)}
-                            </small>
-                          </div>
+                      <div className="past_card">
+                        <div className="description">
+                          <TruncateMarkup lines={3}>
+                            <p>{parse(bill?.description ?? "")}</p>
+                          </TruncateMarkup>
                         </div>
-                        <div className="nay c_flex">
-                          <div className="tomb a_flex">
-                            <i className="fa-regular fa-thumbs-down"></i>
-                            <small>Nay</small>
+                        <div className="yea_nay">
+                          <div className="yea c_flex">
+                            <div className="tomb a_flex">
+                              <i className="fa-regular fa-thumbs-up"></i>
+                              <small>Yea</small>
+                            </div>
+                            <div className="progress_bar">
+                              <Line
+                                percent={bill?.yeaPercentage ?? 0}
+                                strokeWidth={1}
+                                trailWidth={1}
+                                strokeColor="var(--color-bg-green)"
+                              />
+                            </div>
+                            <div className="count_vote">
+                              <small>
+                                {formatNumberShort(bill?.totalYeaVotes ?? 0)}
+                              </small>
+                            </div>
                           </div>
-                          <div className="progress_bar">
-                            <Line
-                              percent={bill?.nayPercentage ?? 0}
-                              strokeWidth={1}
-                              trailWidth={1}
-                              strokeColor="var(--color-red)"
-                            />
-                          </div>
-                          <div className="count_vote">
-                            <small>
-                              {formatNumberShort(bill?.totalNayVotes ?? 0)}
-                            </small>
+                          <div className="nay c_flex">
+                            <div className="tomb a_flex">
+                              <i className="fa-regular fa-thumbs-down"></i>
+                              <small>Nay</small>
+                            </div>
+                            <div className="progress_bar">
+                              <Line
+                                percent={bill?.nayPercentage ?? 0}
+                                strokeWidth={1}
+                                trailWidth={1}
+                                strokeColor="var(--color-red)"
+                              />
+                            </div>
+                            <div className="count_vote">
+                              <small>
+                                {formatNumberShort(bill?.totalNayVotes ?? 0)}
+                              </small>
+                            </div>
                           </div>
                         </div>
                       </div>
@@ -168,7 +173,7 @@ const MoreRelatedCards: React.FC<BillsResponse> = ({ fetchBill, bill }) => {
                 </div>
               </div>
             </div>
-          </div>
+          </Link>
           <div className="comments_share c_flex">
             <div className="a_flex">
               <Link to="" className="comments_count a_flex">
--- frontend/src/components/bills/BillsCard.tsx
+++ frontend/src/components/bills/BillsCard.tsx
@@ -19,126 +19,132 @@ const BillsCard: FC<BillsCardProps> = ({ bill }) => {
   console.log("BILLS:", bill);
   return (
     <div className="post_cards">
-      <div className="featured_card">
-        <div className="slider_content">
-          <div className="slide_list">
-            <div className="list">
-              <div className="img_pallet">
-                <div className="img">
-                  <Link to={`/bills/${bill.slug}`}>
-                    <img src={bill?.image} alt={bill?.title} />
-                  </Link>
-                </div>
-              </div>
-              <div className="details">
-                <div className="past_card">
-                  <div className="tag_date c_flex">
-                    <div className="tags annoying_ta">
-                      <div className="list a_flex">
-                        {bill.sortCategory && (
-                          <span className="tag c_flex">
-                            <small className="bills_f_tag">
-                              {bill?.sortCategory[0]}
-                            </small>
-                          </span>
-                        )}
-                        {bill?.sortStatus && (
-                          <span className="tag c_flex">
-                            <small className="bills_f_tag">
-                              {bill?.sortStatus[0]}
-                            </small>
-                          </span>
-                        )}
-                      </div>
-                    </div>
-                    <div className="date">
-                      <small>{formatDateSlash(bill.createdAt)}</small>
-                    </div>
-                  </div>
-                  <div className="candidate  a_flex">
-                    <div className="img">
-                      <Link
-                        to={`/politician-profile-view/${bill.candidateDetails[0]?._id}`}
-                      >
-                        <img
-                          className="user_candidate_img"
-                          src={bill.candidateDetails[0]?.image}
-                          alt={bill.candidateDetails[0]?.firstName}
-                        />
-                      </Link>
-                    </div>
-                    <div className="name_location">
-                      <div className="name">
-                        <TruncateMarkup lines={1}>
-                          <h5>
-                            <Link
-                              to={`/politician-profile-view/${bill.candidateDetails[0]?._id}`}
-                            >
-                              {bill.candidateDetails[0]?.lastName}{" "}
-                              {bill.candidateDetails[0]?.firstName}
-                            </Link>
-                          </h5>
-                        </TruncateMarkup>
-                      </div>
-                      <div className="location">
-                        <TruncateMarkup lines={1}>
-                          <p>
-                            {bill.candidateDetails[0]?.region},{" "}
-                            {bill.candidateDetails[0]?.stateOfOrigin}
-                          </p>
-                        </TruncateMarkup>
-                      </div>
-                    </div>
+      <Link to={`/bills/${bill.slug}`}>
+        <div className="featured_card">
+          <div className="slider_content">
+            <div className="slide_list">
+              <div className="list">
+                <div className="img_pallet">
+                  <div className="img">
+                    <Link to={`/bills/${bill.slug}`}>
+                      <img src={bill?.image} alt={bill?.title} />
+                    </Link>
                   </div>
                 </div>
+                <div className="details">
+                  <div className="past_card">
+                    <div className="tag_date c_flex">
+                      <div className="tags annoying_ta">
+                        <div className="list a_flex">
+                          {bill.sortCategory && (
+                            <span className="tag c_flex">
+                              <small className="bills_f_tag">
+                                {bill?.sortCategory[0]}
+                              </small>
+                            </span>
+                          )}
+                          {bill?.sortStatus && (
+                            <span className="tag c_flex">
+                              <small className="bills_f_tag">
+                                {bill?.sortStatus[0]}
+                              </small>
+                            </span>
+                          )}
+                        </div>
+                      </div>
+                      <div className="date">
+                        <small>{formatDateSlash(bill.createdAt)}</small>
+                      </div>
+                    </div>
+                    <div className="candidate  a_flex">
+                      <div className="img">
+                        <Link
+                          to={`/politician-profile-view/${bill.candidateDetails[0]?._id}`}
+                        >
+                          <img
+                            className="user_candidate_img"
+                            src={bill.candidateDetails[0]?.image}
+                            alt={bill.candidateDetails[0]?.firstName}
+                          />
+                        </Link>
+                      </div>
+                      <div className="name_location">
+                        <div className="name">
+                          <TruncateMarkup lines={1}>
+                            <h5>
+                              <Link
+                                to={`/politician-profile-view/${bill.candidateDetails[0]?._id}`}
+                              >
+                                {bill.candidateDetails[0]?.lastName}{" "}
+                                {bill.candidateDetails[0]?.firstName}
+                              </Link>
+                            </h5>
+                          </TruncateMarkup>
+                        </div>
+                        <div className="location">
+                          <TruncateMarkup lines={1}>
+                            <p>
+                              {bill.candidateDetails[0]?.region},{" "}
+                              {bill.candidateDetails[0]?.stateOfOrigin}
+                            </p>
+                          </TruncateMarkup>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
 
-                <div className="name">
-                  <Link to={`/bills/${bill.slug}`}>
-                    <TruncateMarkup lines={2}>
-                      <h4>{bill?.title}</h4>
-                    </TruncateMarkup>
-                  </Link>
-                </div>
-
-                <div className="past_card">
-                  <div className="description">
-                    <TruncateMarkup lines={3}>
-                      <p>{parse(bill.description)}</p>
-                    </TruncateMarkup>
+                  <div className="name">
+                    <Link to={`/bills/${bill.slug}`}>
+                      <TruncateMarkup lines={2}>
+                        <h4>{bill?.title}</h4>
+                      </TruncateMarkup>
+                    </Link>
                   </div>
-                  <div className="yea_nay">
-                    <div className="yea c_flex">
-                      <div className="tomb a_flex">
-                        <i className="fa-regular fa-thumbs-up"></i>
-                        <small>Yea</small>
-                      </div>
-                      <div className="progress_bar">
-                        <Line
-                          percent={bill?.yeaPercentage}
-                          strokeWidth={1}
-                          trailWidth={1}
-                          strokeColor="var(--color-bg-green)"
-                        />
-                      </div>
-                      <div className="count_vote">
-                        <small>{formatNumberShort(bill?.totalYeaVotes)}</small>
-                      </div>
+
+                  <div className="past_card">
+                    <div className="description">
+                      <TruncateMarkup lines={3}>
+                        <p>{parse(bill.description)}</p>
+                      </TruncateMarkup>
                     </div>
-                    <div className="nay c_flex">
-                      <div className="tomb a_flex">
-                        <i className="fa-regular fa-thumbs-down"></i>
-                        <small>Nay</small>
+                    <div className="yea_nay">
+                      <div className="yea c_flex">
+                        <div className="tomb a_flex">
+                          <i className="fa-regular fa-thumbs-up"></i>
+                          <small>Yea</small>
+                        </div>
+                        <div className="progress_bar">
+                          <Line
+                            percent={bill?.yeaPercentage}
+                            strokeWidth={1}
+                            trailWidth={1}
+                            strokeColor="var(--color-bg-green)"
+                          />
+                        </div>
+                        <div className="count_vote">
+                          <small>
+                            {formatNumberShort(bill?.totalYeaVotes)}
+                          </small>
+                        </div>
                       </div>
-                      <div className="progress_bar">
-                        <Line
-                          percent={bill?.nayPercentage}
-                          strokeWidth={1}
-                          trailWidth={1}
-                          strokeColor="var(--color-red)"
-                        />
-                      </div>
-                      <div className="count_vote">
-                        <small>{formatNumberShort(bill?.totalNayVotes)}</small>
+                      <div className="nay c_flex">
+                        <div className="tomb a_flex">
+                          <i className="fa-regular fa-thumbs-down"></i>
+                          <small>Nay</small>
+                        </div>
+                        <div className="progress_bar">
+                          <Line
+                            percent={bill?.nayPercentage}
+                            strokeWidth={1}
+                            trailWidth={1}
+                            strokeColor="var(--color-red)"
+                          />
+                        </div>
+                        <div className="count_vote">
+                          <small>
+                            {formatNumberShort(bill?.totalNayVotes)}
+                          </small>
+                        </div>
                       </div>
                     </div>
                   </div>
@@ -147,7 +153,7 @@ const BillsCard: FC<BillsCardProps> = ({ bill }) => {
             </div>
           </div>
         </div>
-      </div>
+      </Link>
       <div className="comments_share c_flex">
         <div className="a_flex">
           <Link to="" className="comments_count a_flex">
